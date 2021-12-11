474167e19495eaf52363d6d4a538275799b93560
a7292969 <a7292969@gmail.com>
2021-12-11 21:25:07 +0200

    move data into database

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    img = models.CharField(max_length=50)
+    type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.type.name}"
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Comment, Product, ProductType
+
 
 def index(req):
-    return render(req, 'index.html')
+    q = req.GET.get("search")
+    if q == "":
+        q = None
+    types = ProductType.objects.all()
+    products = Product.objects.order_by("type").distinct()
+    comments = Comment.objects.all()
+    return render(req, 'index.html', {'prod_types': types, 'products': products, 'comments': comments, 'search': q})
